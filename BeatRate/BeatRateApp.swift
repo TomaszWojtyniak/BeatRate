ee81533dea7d51def58bc0742bbc691caada4531
Tomasz Wojtyniak <wojtyniaktomek@gmail.com>
2025-05-23 18:17:48 +0200

    Init commit

---
+++ BeatRate/BeatRateApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeatRateApp.swift
+//  BeatRate
+//
+//  Created by Tomasz Wojtyniak on 23/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct BeatRateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeatRateTests/BeatRateTests.swift
@@ -0,0 +1,17 @@
+//
+//  BeatRateTests.swift
+//  BeatRateTests
+//
+//  Created by Tomasz Wojtyniak on 23/05/2025.
+//
+
+import Testing
+@testable import BeatRate
+
+struct BeatRateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
