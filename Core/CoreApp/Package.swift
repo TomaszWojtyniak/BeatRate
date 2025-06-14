510668ebde730548eced25b67328de2941fe6c55
Tomasz Wojtyniak <wojtyniaktomek@gmail.com>
2025-06-14 14:51:57 +0200

    Update package swift version

--- Core/CoreApp/Package.swift
+++ Core/CoreApp/Package.swift
@@ -1,10 +1,12 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "CoreApp",
+    defaultLocalization: "en",
+    platforms: [.iOS(.v18)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
